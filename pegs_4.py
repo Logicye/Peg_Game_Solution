6a9a7f08ed21d3336152c131c9af9dba53292465
Logicye <93573184+Logicye@users.noreply.github.com>
2022-06-01 02:44:37 +1000

    First Iteration Additions

---
+++ pegs_4.py
@@ -0,0 +1,21 @@
+import graphs
+import digraphs
+
+# You can define some helper functions here if you like!
+
+def pegsSolution(gameBoard):
+   # Program your solution here
+   
+   # Access functions from the imported files like this:
+   n = graphs.N(V, E, u)
+   t = digraphs.topOrdering(V, E)
+
+   return path
+
+## TEST HARNESS
+# The following will be run if you execute the file like python3 pegs_n1234567.py
+# Your solution should not depend on this code.
+# You may wish to add your own test cases.
+if __name__ == '__main__':
+   gameBoard = 'XoXX' # should return [(3, 'L'), (0, 'R')]
+   print(pegsSolution(gameBoard))
